b81f7e79479560a37dac87bcb8ca78892ef45b0f
Agamali <muhammad.agamalieff@yandex.ru>
2023-12-11 17:09:32 +0500

    Ошибки линта 7.1 ветка

--- js/module.js
+++ js/module.js
@@ -1,20 +1,20 @@
-import { generatePhotoArray } from "./data.js";
-import { openPicture } from "./openBigPic.js";
+import { generatePhotoArray } from './data.js';
+import { openPicture } from './openbigPic.js';
 
 export const getRenderedCards = () => {
   const pictureTemplate = document
-    .querySelector("#picture")
-    .content.querySelector(".picture");
-  const picturesContainer = document.querySelector(".pictures");
+    .querySelector('#picture')
+    .content.querySelector('.picture');
+  const picturesContainer = document.querySelector('.pictures');
   const randomPosts = generatePhotoArray(25);
   const picturesFragment = document.createDocumentFragment();
   randomPosts.forEach(({ url, description, likes, comments }) => {
     const picture = pictureTemplate.cloneNode(true);
-    picture.querySelector("img").src = url;
-    picture.querySelector("img").alt = description;
-    picture.querySelector(".picture__likes").textContent = likes;
-    picture.querySelector(".picture__comments").textContent = comments.length;
-    picture.addEventListener("click", (evt) => {
+    picture.querySelector('img').src = url;
+    picture.querySelector('img').alt = description;
+    picture.querySelector('.picture__likes').textContent = likes;
+    picture.querySelector('.picture__comments').textContent = comments.length;
+    picture.addEventListener('click', (evt) => {
       openPicture(evt, url, description, likes, comments);
     });
     picturesFragment.append(picture);
